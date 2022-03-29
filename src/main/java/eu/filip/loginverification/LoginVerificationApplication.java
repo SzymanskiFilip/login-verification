58e9dfea1fc31dc0db8f51c44a408653dffe3d61
Filip <szymanski.fp@gmail.com>
2022-03-29 21:48:48 +0200

    default files

--- src/main/java/eu/filip/loginverification/LoginVerificationApplication.java
+++ src/main/java/eu/filip/loginverification/LoginVerificationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginVerificationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginVerificationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoginVerificationApplication.class, args);
+    }
 
 }
