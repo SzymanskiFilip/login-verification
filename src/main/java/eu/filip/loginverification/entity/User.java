8df12794d6f54d5c9d4b9f4605fb2cb668a8ae0b
Filip <szymanski.fp@gmail.com>
2022-03-29 21:49:06 +0200

    added user entity

--- src/main/java/eu/filip/loginverification/entity/User.java
+++ src/main/java/eu/filip/loginverification/entity/User.java
@@ -1,2 +1,34 @@
-package eu.filip.loginverification.entity;public class User {
+package eu.filip.loginverification.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "username", length = 60)
+    private String username;
+
+    @Column(name = "password", length = 60)
+    private String password;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "active")
+    private boolean active;
+
 }
