d052407d79c6c78f3c13baa552c316046adafb2f
Filip <szymanski.fp@gmail.com>
2022-03-29 21:37:24 +0200

    initialized

---
+++ src/main/java/eu/filip/loginverification/LoginVerificationApplication.java
@@ -0,0 +1,13 @@
+package eu.filip.loginverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/filip/loginverification/LoginVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.filip.loginverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
