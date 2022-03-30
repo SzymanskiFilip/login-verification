92cce26806fea5f3b74964689d870ade761520a3
Filip <szymanski.fp@gmail.com>
2022-03-30 22:25:21 +0200

    added register that expects valid credentials

--- src/main/java/eu/filip/loginverification/controller/RegistrationController.java
+++ src/main/java/eu/filip/loginverification/controller/RegistrationController.java
@@ -1,2 +1,19 @@
-package eu.filip.loginverification.controller;public class RegistrationController {
+package eu.filip.loginverification.controller;
+
+import eu.filip.loginverification.util.RegisterCredentials;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Slf4j
+public class RegistrationController {
+
+    @PostMapping("/register")
+    public void register(@RequestBody @Valid RegisterCredentials registerCredentials){
+        log.info(registerCredentials.toString());
+    }
 }
