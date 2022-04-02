a8e9e737ed71c84a5cfe36ec5070d56de79e395a
Filip <szymanski.fp@gmail.com>
2022-04-02 22:43:06 +0200

    added mail to project

---
+++ src/main/java/eu/filip/loginverification/config/MailConfig.java
@@ -0,0 +1,18 @@
+package eu.filip.loginverification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername();
+    }
+}
